d4ce2fb907d19283ec6ecfef0bd6a3efcbcd2769
Toni A <tamengual9@gmail.com>
2019-05-29 17:07:05 +0200

    fent http i web api in memory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,10 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 
 //http
-import { HttpClientModule }    from '@angular/common/http';
-
+import { HttpClientModule } from '@angular/common/http';
+//in-memory WEB API -> for HTTP requests
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { CiclistesComponent } from './ciclistes/ciclistes.component';
 import { CiclistaDetallComponent } from './ciclista-detall/ciclista-detall.component';
@@ -23,7 +25,18 @@ import { DashboardComponent } from './dashboard/dashboard.component'; // <-- NgM
     CiclistesComponent,
     CiclistaDetallComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+
+    HttpClientModule,
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
+
+
   ],
   imports: [
     BrowserModule,
